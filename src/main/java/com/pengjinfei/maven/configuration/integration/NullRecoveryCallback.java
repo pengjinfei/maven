b6b22f92563af552e684122d83eab7d34910c375
pengjinfei <pengjinfei@126.com>
2017-08-23 20:57:11 +0800

    AsynRunner Annotation add delayer

---
+++ src/main/java/com/pengjinfei/maven/configuration/integration/NullRecoveryCallback.java
@@ -0,0 +1,21 @@
+package com.pengjinfei.maven.configuration.integration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.support.GenericMessage;
+import org.springframework.retry.RecoveryCallback;
+import org.springframework.retry.RetryContext;
+
+/**
+ * Created on 8/23/17
+ *
+ * @author Pengjinfei
+ */
+@Slf4j
+public class NullRecoveryCallback implements RecoveryCallback {
+    @Override
+    public Object recover(RetryContext retryContext) throws Exception {
+        GenericMessage message = (GenericMessage) retryContext.getAttribute("message");
+        log.info("retry message {} out!",message);
+        return null;
+    }
+}
